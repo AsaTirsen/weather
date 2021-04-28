0a9f6beaa2792f95c17f79814e5458d4a00922c5
Asa Tirsen <asa.tirsen@gmail.com>
2021-04-28 09:50:19 +0200

    changed navbar

--- config/weather/geoip.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    "API-key" => "52ca019cf35f1e86f782ed09916211c8",
-    "url" => "http://api.ipstack.com/"
-];
--- config/weather/weather.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    "API-key" => "c7a66a573b79dd6c14f14a2e93ed0d11",
-    "url" => "http://api.openweathermap.org/data/2.5/onecall"
-];
