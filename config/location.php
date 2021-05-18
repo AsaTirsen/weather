d1296cf501c20b435dc234632646cdcb2fc132ec
Asa Tirsen <asa.tirsen@gmail.com>
2021-05-18 11:29:30 +0200

    rmd api keys

--- config/location.php
+++ config/location.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    "locationAPI-key" => "52ca019cf35f1e86f782ed09916211c8",
+    "locationAPI-key" => "",
     "url" => "http://api.ipstack.com/"
 ];
--- config/weather.php
+++ config/weather.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    "weatherAPI-key" => "c7a66a573b79dd6c14f14a2e93ed0d11",
+    "weatherAPI-key" => "",
     "url" => "http://api.openweathermap.org/data/2.5/onecall"
 ];
